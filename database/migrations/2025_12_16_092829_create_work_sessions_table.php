522456b1ae1781cb594086dfa65de57b7fd237c4
Pedro Jr Solatorio <solatoriop@gmail.com>
2025-12-16 20:33:06 +0800

    Initial commit

---
+++ database/migrations/2025_12_16_092829_create_work_sessions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->integer('target_minutes');
+            $table->integer('worked_minutes')->default(0);
+            $table->enum('status', ['active', 'paused', 'completed'])->default('paused');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_sessions');
+    }
+};
