9e2d7adb166acc969b87eacfd563694948f15af7
Pedro Jr Solatorio <solatoriop@gmail.com>
2025-12-16 21:50:52 +0800

    add fillables and relationship in model

--- app/Models/TimeLog.php
+++ app/Models/TimeLog.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class TimeLog extends Model
 {
-    //
+    protected $fillable = [
+        'work_session_id',
+        'start_time',
+        'end_time',
+        'duration_minutes'
+    ];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime'
+    ];
+
+    public function workSession()
+    {
+        return $this->belongsTo(WorkSession::class);
+    }
 }
--- app/Models/WorkSession.php
+++ app/Models/WorkSession.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class WorkSession extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'target_minutes',
+        'worked_minutes',
+        'status',
+        'current_start_time'
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'current_start_time' => 'datetime'
+    ];
+
+    public function timeLogs()
+    {
+        return $this->hasMany(TimeLog::class);
+    }
 }
